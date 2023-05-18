7e779b2acd5bc4769009a5873a0e30132b0039aa
Paweł Protaś <pawciopr@gmail.com>
2023-05-18 11:58:01 +0200

    Add config-sample.js with coreUrl, clubUrl and comment

--- src/config-sample.js
+++ src/config-sample.js
@@ -1,5 +1,11 @@
+/**
+ * Make a copy of this file in the same src directory and name it config.js. Set values for fields.
+ * coreUrl - URL address to the directory where the files from the assets directory will be placed.
+ * clubUrl - The URL to the server from which the club API will be used.
+ */
 export default function config() {
     return {
-        coreUrl: ''
+        coreUrl: '',
+        clubUrl: ''
     };
 }
