ccdf336a4e5582b5e4efafc4c613c599ea6398cd
Paweł Protaś <pawciopr@gmail.com>
2023-05-22 12:03:48 +0200

    Change conditions and tip message

--- src/game/game-stats/tips.js
+++ src/game/game-stats/tips.js
@@ -11,16 +11,16 @@ export class Tip {
   get currentTip() {
     this.currentTip =
       "Twój wynik nie jest najlepszy. Chyba jeszcze nie jesteś na tym etapie. Spróbuj zagrać na prostszym poziomie.";
-    if (this.points > 120) {
+    if (this.points > 200) {
       this.currentTip =
         "Jesteś mistrzem! Nawet nam jest trudno osiągnąć tyle punktów. Koniecznie pochwal się swoim wynikiem w komentarzach lub napisz na naszej stronie na Facebook-u!";
-    } else if (this.points > 100) {
+    } else if (this.points > 150) {
       this.currentTip =
         "Wow! Świetny wynik! Jeśli udało ci się go osiągnąć, twój mózg pracuje na wysokich obrotach. Koniecznie pochwal się tym wynikiem w komentarzach lub napisz na naszej stronie na Facebook-u!";
-    } else if (this.points > 50) {
+    } else if (this.points > 100) {
       this.currentTip =
-        "Świetna robota! Właśnie o to chodzi. Ciekawe czy potrafisz pokonać barierę 100 punktów?";
-    } else if (this.points > 30) {
+        "Świetna robota! Właśnie o to chodzi. Ciekawe czy potrafisz pokonać barierę 150 punktów?";
+    } else if (this.points > 50) {
       this.currentTip = "Idzie Ci co raz lepiej - ćwicz dalej!";
     } else if (this.points > 10) {
       this.currentTip = "Wiesz już o co chodzi. Nie przestawaj - ćwicz dalej!";
