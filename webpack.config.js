0170a9ea0810da7d18ca93f805bc47bb2d793dc3
isccown125 <92261514+isccown125@users.noreply.github.com>
2023-05-25 12:32:13 +0200

    Change game structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,12 @@ module.exports = {
   entry: path.resolve(__dirname, "./src/index.js"),
   mode: "development",
   devServer: {
+    static: {
+      directory: path.resolve(__dirname),
+    },
     port: 3001,
   },
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
